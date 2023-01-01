7aea53d9aa7f4f4da08e4507ad946a670eea6a3b
Nauany Costa <nauany.costa@yum.com>
2023-01-01 20:21:45 -0300

    style: add dark filter to background image

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -2,7 +2,9 @@ import styled from "styled-components";
 import img from "./../../assets/weeb-image.png";
 
 export const Container = styled.div`
-  background: url("${img}");
+  background-image: linear-gradient(rgba(3, 3, 3, 0.5), rgba(3, 3, 3, 0.5)),
+    url("${img}");
+
   background-repeat: no-repeat;
   background-size: contain;
   background-size: cover;
